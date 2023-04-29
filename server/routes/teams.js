df3935039bcb3c44128f2e78fb0b80c74baeba32
boburf <changeitgo3@gmail.com>
2023-04-30 02:04:00 +0500

    feat: team delete

--- server/routes/teams.js
+++ server/routes/teams.js
@@ -4,5 +4,6 @@ const controll = require("../controller/teams.js")
 router.post("/create", controll.create)
 router.put("/update/:id", controll.update)
 router.get("/", controll.find)
+router.delete("/:id", controll.delete)
 
 module.exports = router
